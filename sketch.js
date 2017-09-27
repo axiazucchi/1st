72c7bde65d7cc39ded8d0f299b3db5e9b7269751
axiazucchi <32325197+axiazucchi@users.noreply.github.com>
2017-09-27 12:59:13 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  
+  createCanvas(600,400);
+	background(220,220,220);
+	
+}
+
+function draw() {
+  
+  noStroke();
+  fill(255,180,0,50);
+  ellipse(mouseX,mouseY,50,50);
+  
+}
